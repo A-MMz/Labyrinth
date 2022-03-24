8a5132540bc3bb417a103ce6fa2411c57dca3741
AbrahamMMz <79181545+AbrahamMMz@users.noreply.github.com>
2022-03-23 20:29:07 -0600

    finish?

--- preactice.py
+++ preactice.py
@@ -3,12 +3,12 @@ import pygame
 import time
 visited = []
 stack1 = []
-
-cellSize = 50
-gridSize = 10
+#  12 < cellsize < ∞
+cellSize = 30
+gridSize = 25
 
 EDGE_LENGHT = cellSize*gridSize
-EDGE = cellSize/2
+EDGE = round(cellSize/2)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
@@ -21,24 +21,24 @@ def is_inbounds(x,y):
 
 def drawGrid():
     for i in range(1,gridSize):
-        pygame.draw.line(screen,WHITE,((i*cellSize)+EDGE,EDGE),((i*cellSize)+EDGE,EDGE_LENGHT+EDGE),3)
-        pygame.draw.line(screen,WHITE,(EDGE,(i*cellSize)+EDGE),(EDGE_LENGHT+EDGE,(i*cellSize)+EDGE),3)
+        pygame.draw.line(screen,WHITE,((i*cellSize)+EDGE,EDGE),((i*cellSize)+EDGE,EDGE_LENGHT+EDGE),round(cellSize*0.04))
+        pygame.draw.line(screen,WHITE,(EDGE,(i*cellSize)+EDGE),(EDGE_LENGHT+EDGE,(i*cellSize)+EDGE),round(cellSize*0.04))
 #------------Movement---------------------------
 def m_North(x,y):
     #                      |inicial: X          Y     | Width       Height     |
-    pygame.draw.rect(screen,BLACK,[x+(cellSize/100), y+(cellSize/100)-cellSize, cellSize-(cellSize/100), cellSize*2-((cellSize/100)*2)])
+    pygame.draw.rect(screen,BLACK,[x+(round(cellSize*0.04)), y+(round(cellSize*0.04))-cellSize, cellSize-(round(cellSize*0.04)), cellSize*2-((round(cellSize*0.04))*2)])
     #pygame.draw.rect(screen,BLACK,[x, y-cellSize, cellSize, cellSize*2])
     pygame.display.update()
 def m_South(x,y):
-    pygame.draw.rect(screen,BLACK,[x+(cellSize/100), y+(cellSize/100), cellSize-(cellSize/100), (cellSize*2)-((cellSize/100)*2)])
+    pygame.draw.rect(screen,BLACK,[x+(round(cellSize*0.04)), y+(round(cellSize*0.04)), cellSize-(round(cellSize*0.04)), (cellSize*2)-((round(cellSize*0.04))*2)])
     #pygame.draw.rect(screen,BLACK,[x, y, cellSize, cellSize*2])
     pygame.display.update()
 def m_East(x,y):
-    pygame.draw.rect(screen,BLACK,[x+(cellSize/100), y+(cellSize/100), (cellSize*2)-((cellSize/100)*2), cellSize-(cellSize/100)])
+    pygame.draw.rect(screen,BLACK,[x+(round(cellSize*0.04)), y+(round(cellSize*0.04)), (cellSize*2)-((round(cellSize*0.04))*2), cellSize-(round(cellSize*0.04))])
     #pygame.draw.rect(screen,BLACK,[x, y, cellSize*2, cellSize])
     pygame.display.update()
 def m_West(x,y):
-    pygame.draw.rect(screen,BLACK,[x+(cellSize/100)-cellSize, y+(cellSize/100), (cellSize*2)-((cellSize/100)*2), cellSize-(cellSize/100)])  
+    pygame.draw.rect(screen,BLACK,[x+(round(cellSize*0.04))-cellSize, y+(round(cellSize*0.04)), (cellSize*2)-((round(cellSize*0.04))*2), cellSize-(round(cellSize*0.04))])  
     #pygame.draw.rect(screen,BLACK,[x-cellSize, y, cellSize*2, cellSize]) 
     pygame.display.update()
 
@@ -48,7 +48,7 @@ def maze_creator(x,y):
     stack1.append((x,y))
     visited.append((x,y))
     while len(stack1) > 0:
-        time.sleep(2)
+        #time.sleep(1)
         directions = []
         if(is_inbounds(x,y-cellSize) and (x,y-cellSize) not in visited):
             directions.append("N")
@@ -83,7 +83,7 @@ def maze_creator(x,y):
                 visited.append((x, y))
                 stack1.append((x, y))
         else:
-            time.sleep(2)
+            #time.sleep(1)
             x, y = stack1.pop()
 
 def main():
@@ -95,11 +95,11 @@ def main():
     pygame.display.set_caption("minimal program")
      
     screen = pygame.display.set_mode((EDGE_LENGHT+(EDGE*2),EDGE_LENGHT+(EDGE*2)))
-    pygame.draw.rect(screen,WHITE,[EDGE,EDGE,(EDGE_LENGHT+2),(EDGE_LENGHT+2)],3)
+    pygame.draw.rect(screen,WHITE,[EDGE,EDGE,(EDGE_LENGHT+(cellSize*.04)),(EDGE_LENGHT+(cellSize*.04))],3)
     drawGrid()
-    maze_creator(25,25)
-    pygame.draw.rect(screen,WHITE,[EDGE-10,EDGE+2,cellSize-2,cellSize-2],3)
-    pygame.draw.rect(screen,WHITE,[EDGE_LENGHT-EDGE/2,EDGE_LENGHT+EDGE-cellSize+2,cellSize-2,cellSize-2],3)
+    maze_creator(EDGE,EDGE)
+    pygame.draw.rect(screen,BLACK,[EDGE-10,EDGE+(cellSize*.04),cellSize-(cellSize*.04),cellSize-(cellSize*.04)])
+    pygame.draw.rect(screen,BLACK,[EDGE_LENGHT-EDGE/2,EDGE_LENGHT+EDGE-cellSize+(cellSize*.04),cellSize-(cellSize*0.04),cellSize-(cellSize*0.04)])
     running = True
 
     print(is_inbounds(24,EDGE_LENGHT))
